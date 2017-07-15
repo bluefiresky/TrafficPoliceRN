10c4c5eff6b440c15d31971d253018d7cef268f4
wuran <wuran@ucfgroup.com>
2017-07-15 17:46:25 +0800

    perfech ap home page

--- app/views/assistPolice/apHomePageView.js
+++ app/views/assistPolice/apHomePageView.js
@@ -2,15 +2,20 @@
 * 确认事故信息
 */
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, Image, FlatList,Platform,TouchableHighlight } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image, FlatList,Platform,TouchableHighlight,InteractionManager,RefreshControl } from "react-native";
 import { connect } from 'react-redux';
 
-import { W, H, backgroundGrey,formLeftText, formRightText, mainBule } from '../../configs/index.js';/** 自定义配置参数 */
-import { ProgressView } from '../../components/index.js';  /** 自定义组件 */
+import { W, H, backgroundGrey,formLeftText, formRightText, mainBule, Version } from '../../configs/index.js';/** 自定义配置参数 */
+import { ProgressView, UpdateModal } from '../../components/index.js';  /** 自定义组件 */
 import * as Contract from '../../service/contract.js'; /** api方法名 */
 import { create_service } from '../../redux/index.js'; /** 调用api的Action */
 import { getStore } from '../../redux/index.js';       /** Redux的store */
 import { XButton } from '../../components/index.js';  /** 自定义组件 */
+import { StorageHelper, Utility } from '../../utility/index.js';
+
+const AppType = Platform.OS === 'ios'?2:1;
+const HomeBgH = (W * 749)/1125;
+const InfoH = Platform.OS === 'ios'?(HomeBgH-30-20):(HomeBgH-15-20)
 
 class ApHomePageView extends Component {
 
@@ -23,23 +28,36 @@ class ApHomePageView extends Component {
       showWaitUpload: true,
       showNoComplete: true,
       showHistoryCase: true,
-      loading: false
+      loading: false,
+      isRefreshing: false,
+      showForceUpdate: false, // 强制更新提示出现判断
+      forceUpdateParams: null,   // 强制更新所需参数<appDownloadUrl, upgradeReason>
     }
+
+    this.forceUpdate = null;
+    this.dictionary = null;
+    this.unCompleteSum = 0;
+    this.unUploadedSum = 0;
+    this._getData = this._getData.bind(this);
+    this._onRefresh = this._onRefresh.bind(this);
   }
   componentDidMount() {
     //1、载入页面，加载账户信息、大队章、勘察章、字典表（保险公司、天气、车辆类型等）信息，加载过程显示loading，信息缓存到本地。2、每次启动APP，请求后台字典数据是否有更新，如果有更新，后台返回新的字典数据，客户端缓存最新字典数据；如果没有更新，不需重新缓存数据。若无网络，弹框提示“未检测到网络，是否离线处理？”点击继续。
+    InteractionManager.runAfterInteractions(async () => {
+      this._getData();
+    });
   }
   //历史案件
   gotoHistoryCase(type){
     switch (type) {
       case 'WaitUpload':
-        this.props.navigation.navigate('HistoricalCaseView',{title:'已完结案件'});
+        this.props.navigation.navigate('HistoricalCaseView',{title:'待上传案件', type: 1});
         break;
       case 'NoComplete':
-        this.props.navigation.navigate('HistoricalCaseView',{title:'待上传案件'});
+        this.props.navigation.navigate('HistoricalCaseView',{title:'未完结案件', type: 2});
         break;
       case 'HistoryCase':
-        this.props.navigation.navigate('HistoricalCaseView',{title:'未完结案件'});
+        this.props.navigation.navigate('HistoricalCaseView',{title:'已完结案件', type: 3});
         break;
       default:
 
@@ -49,31 +67,47 @@ class ApHomePageView extends Component {
   handleCase(){
     this.props.navigation.navigate('AAccidentBasicInformationView');
   }
-  renderRowItem(title,value){
+
+  renderRowItem(title,value,marginTop){
     return (
-      <View style={{flexDirection:'row',marginLeft:15,marginTop:10,marginBottom:10}}>
-        <Text style={{fontSize:13,color:formLeftText}}>{title}</Text>
-        <Text style={{fontSize:13,color:formLeftText}}>{value}</Text>
+      <View style={{flexDirection:'row',marginTop,alignItems:'center'}}>
+        <Text style={{fontSize:16,color:formRightText}}>{title}</Text>
+        <Text style={{fontSize:16,color:formRightText}}>{value}</Text>
       </View>
     )
   }
+
   render(){
     return(
-      <ScrollView style={styles.container}
-                   showsVerticalScrollIndicator={false}>
-        <Image source={require('./image/home_bg.png')} style={{width:W, height:W / 1.5,overflow:'visible'}} resizeMode="contain">
-          <Text style={{color:'#ffffff',fontSize:18,alignSelf:'center',marginTop:(Platform.OS === 'ios') ? 30 : 10,backgroundColor:'transparent'}}>首页</Text>
+      <ScrollView
+        style={styles.container}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.isRefreshing}
+            onRefresh={this._onRefresh}
+            tintColor="#1174D9"
+            title="刷新中..."
+            titleColor={formLeftText}
+            colors={['#1174D9', '#1174D9', '#1174D9']}
+            progressBackgroundColor="white"
+          />
+        }
+        showsVerticalScrollIndicator={false}>
+        <Image source={require('./image/home_bg.png')} style={{width:W, height:HomeBgH,overflow:'visible'}} resizeMode="contain">
+          <Text style={{color:'#ffffff',fontSize:18,alignSelf:'center',marginTop:(Platform.OS === 'ios') ? 30 : 15,backgroundColor:'transparent'}}>首页</Text>
+          <TouchableHighlight style={{top:Platform.OS === 'ios'? 30 : 10,right:15,position:'absolute'}} onPress={()=>{this.props.navigation.navigate('SettingView')}} underlayColor={'transparent'}>
+            <Image source={require('./image/setting.png')} style={{width:20,height:20}}/>
+          </TouchableHighlight>
           <View style={{marginTop:15,marginLeft:15,width:W-30,backgroundColor:'#ffffff',borderRadius:10}}>
              <View style={{flexDirection:'row',marginLeft:20,marginTop:20}}>
-               <Text style={{fontSize:22,color:formLeftText,fontWeight:'bold'}}>
-                 张扬
-               </Text>
+               <Text style={{fontSize:22,color:formLeftText,fontWeight:'bold'}}>{global.personal.policeName}</Text>
              </View>
              <View style={{flexDirection:'row',marginTop:20,marginLeft:20,marginBottom:20,justifyContent:'space-between'}}>
                <View style={{backgroundColor:'#ffffff'}}>
-                 {this.renderRowItem('手机号：','13318767654')}
-                 {this.renderRowItem('所属城市：','河北')}
-                 {this.renderRowItem('所属大队：','某某某大队')}
+               {this.renderRowItem('手机号：', global.personal.mobile, 0)}
+               {this.renderRowItem('警员编号：', global.personal.policeNumber, 10)}
+               {this.renderRowItem('所属城市：', global.personal.cityName, 10)}
+               {this.renderRowItem('所属大队：', global.personal.depName, 10)}
                </View>
              </View>
           </View>
@@ -86,7 +120,7 @@ class ApHomePageView extends Component {
                待上传案件
              </Text>
              <Text style={{fontSize:13,color:formRightText,marginTop:10}}>
-               您有xx起案件未上传，请及时上传
+               {`您有${this.unUploadedSum}起案件未上传，请及时上传`}
              </Text>
            </View>
            <Image source={require('./image/right_arrow.png')} style={{width:7,height:12,marginRight:15,alignSelf:'center'}}/>
@@ -100,7 +134,7 @@ class ApHomePageView extends Component {
                未完结案件
              </Text>
              <Text style={{fontSize:13,color:formRightText,marginTop:10}}>
-               您有xx起案件未处理完，请及时处理
+               {`您有${this.unCompleteSum}起案件未处理完，请及时处理`}
              </Text>
            </View>
            <Image source={require('./image/right_arrow.png')} style={{width:7,height:12,marginRight:15,alignSelf:'center'}}/>
@@ -116,19 +150,62 @@ class ApHomePageView extends Component {
          </View>
        </TouchableHighlight>:null}
        <TouchableHighlight onPress={()=> this.handleCase()} underlayColor={'transparent'}>
-         <Image source={require('./image/handle_case.png')} style={{marginTop:20,alignSelf:'center',width:150,height:150}}>
-          <Text style={{fontSize:14,color:'#ffffff',marginTop:95,backgroundColor:'transparent',alignSelf:'center'}}>
+         <Image source={require('./image/handle_case.png')} style={{marginTop:20,alignSelf:'center',width:130,height:130}}>
+          <Text style={{fontSize:14,color:'#ffffff',marginTop:72,backgroundColor:'transparent',alignSelf:'center'}}>
             处理案件
           </Text>
          </Image>
        </TouchableHighlight>
-        <TouchableHighlight style={{top:Platform.OS === 'ios'? 30 : 10,right:15,position:'absolute'}} onPress={()=>{this.props.navigation.navigate('SettingView')}} underlayColor={'transparent'}>
-          <Image source={require('./image/setting.png')} style={{width:20,height:20}}/>
-        </TouchableHighlight>
+
         <ProgressView show={this.state.loading}/>
+        <UpdateModal show={this.state.showForceUpdate} content={this.state.forceUpdateParams}/>
       </ScrollView>
 
     );
+  }
+
+  /** Private **/
+  async _getData(refresh){
+    if(refresh) this.setState({isRefreshing: true});
+    else this.setState({loading: true});
+
+    // 查看信分期的强制更新跳转，逻辑：isUpgrade -> [isAllUpgrade -> depCodes]
+    this.forceUpdate = await this.props.dispatch( create_service(Contract.POST_FORCE_UPDATE, {appType: AppType, appVersion: Version}));
+    // console.log('PpHomePageView execute _getData this.forceUpdate -->> ', this.forceUpdate);
+    if(this._checkUpdate(this.forceUpdate)) return;
+
+    // 字典需要上传本地的版本
+    let v = getStore().getState().dictionary.version;
+    this.dictionary = await this.props.dispatch( create_service(Contract.POST_ACHIEVE_DICTIONARY,{v}))
+    // console.log('PpHomePageView execute _getData this.dictionary -->> ', this.dictionary);
+
+    this.unCompleteSum = await StorageHelper.getUnCompletedCaseSum();
+    this.unUploadedSum = await StorageHelper.getUnUploadedCaseSum();
+
+    if(refresh) this.setState({isRefreshing: false});
+    else this.setState({loading: false});
+  }
+
+  _checkUpdate(data){
+    if(!data) return false;
+    let { isUpgrade, isAllUpgrade, depCodes, appDownloadUrl, upgradeReason } = data;
+    if(isUpgrade === 2){
+      if(isAllUpgrade === 2){
+        this.setState({showForceUpdate: true, forceUpdateParams:{appDownloadUrl, upgradeReason}})
+        return true;
+      }else if(isAllUpgrade === 1){
+        let tmp = depCodes.split(',');
+        if(tmp.indexOf(global.personal.depCode) != -1){
+          this.setState({showForceUpdate: true, forceUpdateParams:{appDownloadUrl, upgradeReason}})
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  _onRefresh(){
+    this._getData(true);
   }
 
 }
