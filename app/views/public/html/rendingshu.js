36726df9a6a0a0dc20207729e222f902221417c3
wuran <wuran@ucfgroup.com>
2017-08-04 10:33:32 +0800

    fix bug on instrunce

--- app/views/public/html/rendingshu.js
+++ app/views/public/html/rendingshu.js
@@ -5,6 +5,7 @@ export function generateRDS(basic, personList, fact, responsibility, conciliatio
      <head lang="en">
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+      <meta name="format-detection" content="telephone=no"/>
       <title></title>
       <style>
         p{text-indent: 2em;}
