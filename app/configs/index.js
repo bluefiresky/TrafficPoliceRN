af6165698e660f336d94b9c2ae2b9a62527b572d
wuran <wuran@ucfgroup.com>
2017-08-25 11:18:27 +0800

    package to 2.2

--- app/configs/index.js
+++ app/configs/index.js
@@ -5,5 +5,5 @@
 
 export * from "./XinColors.js"; /** 应用所需基础颜色  */
 export * from "./XinStyles.js"; /** 应用样式相关所需基础参数 */
-export * from "./service/XinConfig.dev.js"; /** Dev环境---服务相关配置，e.g: url... */
-// export * from "./service/XinConfig.prod.js"; /** Prod环境---服务相关配置，e.g: url... */
+// export * from "./service/XinConfig.dev.js"; /** Dev环境---服务相关配置，e.g: url... */
+export * from "./service/XinConfig.prod.js"; /** Prod环境---服务相关配置，e.g: url... */
